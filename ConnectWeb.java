c4ff91ffa956d876f39b0a2de949eefc7ebe0a22
benjmain1021523 <s1021523@mail.yzu.edu.tw>
2019-08-14 11:39:17 +0800

    加上可從CMD輸入目標網址的部分

--- ConnectWeb.java
+++ ConnectWeb.java
@@ -5,10 +5,36 @@ import javax.net.ssl.HttpsURLConnection;
 
 public class ConnectWeb{
     public static void main(String[] args){
-        System.out.println("******HTTP part******");
-        http();
-        System.out.println("\n******HTTPS part******");
-        https();
+        if(args.length == 0){
+            System.out.println("範例：");
+            System.out.println("******HTTP part******");
+            http();
+            System.out.println("\n******HTTPS part******");
+            https();
+        }
+        else{
+            for(String i:args){
+                if(i.substring(0, 5) == "https"){
+                    https(i);
+                }
+                else{
+                    http(i);
+                }
+            }
+        }
+    }
+    static void https(String target){
+        try {
+            URL url = new URL(target);
+            HttpsURLConnection con = (HttpsURLConnection)url.openConnection();
+
+            String TextContent = readAll(con.getInputStream(), "UTF-8");
+
+            System.out.println(TextContent);
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
     }
     static void https(){
         String target = "https://gist.githubusercontent.com/Benjamin1021523/1701eb96206b779e1595ce0cc3f82466/raw/fc71033195bdec93457f39fe26cbef74701dcce2/Example.java";
@@ -19,6 +45,19 @@ public class ConnectWeb{
             String TextContent = readAll(con.getInputStream(), "UTF-8");
 
             System.out.println(TextContent);
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
+    }
+    static void http(String target){
+        try{
+            URL url = new URL(target);
+            InputStream stream = url.openStream();
+
+            String TextContent = readAll(stream, "UTF-8");
+
+            System.out.print(TextContent);
         }
         catch(Exception e){
             System.out.println(e);
